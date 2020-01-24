e43015018538332b683270f6cce89bd331d14c7c
Christian Paling <christian.paling@googlemail.com>
2020-01-24 21:53:30 +0100

    Delay evaluation of datasource until it is actually used

--- src/tinytimer/db.clj
+++ src/tinytimer/db.clj
@@ -2,5 +2,13 @@
   (:require [next.jdbc :as jdbc]
             [environ.core :refer [env]]))
 
-(def datasource
-  (jdbc/get-datasource (env :database-url)))
+;; `datasource` should not be evaluated when this file is parsed e.g. during
+;; compile time. It should be evaluated when it is actually used.
+;; Therefore, the creation is delayed and access to the datasource
+;; is only provided via `get-datasource`
+(def ^:private datasource
+  (delay (jdbc/get-datasource (env :database-url))))
+
+(defn get-datasource
+  []
+  @datasource)
